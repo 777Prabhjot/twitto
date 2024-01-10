def6f84e227ddf25b76d16f989d5a079b4eb2ea6
Prabhjot <777prabhjot@gmail.com>
2024-01-10 16:14:09 +0530

    fix: fixed trpc base url

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -11,7 +11,7 @@ const Providers = ({ children }: PropsWithChildren) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: `http://localhost:3000/api/trpc`,
+          url: `https://twittoo.vercel.app/api/trpc`,
           fetch(url, options) {
             return fetch(url, {
               ...options,
