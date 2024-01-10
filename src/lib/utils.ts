4e58a7b7db5acc8a6b5b4ec6b0ec36873a1279af
Prabhjot <777prabhjot@gmail.com>
2024-01-10 16:09:09 +0530

    changed base url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,5 +6,5 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function absoluteUrl(path: string) {
-  return `${process.env.NEXTAUTH_URL || "http://localhost:3000/"}${path}`;
+  return `${process.env.NEXTAUTH_URL || "https://twittoo.vercel.com/"}${path}`;
 }
